00acc17b7a80868f85da6b32fd7ae855bcae2f05
houli <houli@wesai.com>
2018-08-15 13:29:12 +0800

    add 创建工程
    add

---
+++ HLKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLKit
+//
+//  Created by houli on 2018/8/15.
+//  Copyright © 2018年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HLKit
+//
+//  Created by houli on 2018/8/15.
+//  Copyright © 2018年 com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
